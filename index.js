5fea4088e6f6efbd2bb1e00a8ffdf7d23681925e
AkramAlJabari <akramm3810@gmail.com>
2022-09-26 22:07:56 +0300

    .

--- index.js
+++ index.js
@@ -66,7 +66,6 @@ app.get(`/`, async function (req, res) {
   let Updates = await require('./models/Updates').find({})
   if (Updates[0].Updates.length == 0) Updates[0].Updates = [`لا يوجد تحديثات`]
   let Comments = await require('./models/Comments').find({})
-  if (Comments[0].Comments.length == 0) Comments[0].Comments = [`لا يوجد تعليقات`]
   fetch('https://api.ipify.org/?format=json')
     .then((response) => response.json())
     .then(async (data) => {
@@ -79,7 +78,7 @@ app.get(`/`, async function (req, res) {
           comments: Comments[0].Comments,
         })
       } else {
-        require('./models/Users_Count').updateOne({ ID: `1` }, { $push: { Users: data.ip } }, function (err, res) { })
+        require('./models/Users_Count').updateOne({ ID: `1` }, { $push: { Users: data.ip } }, function (err) { })
         res.render(`index`, {
           usernew: true,
           users_count: Users_Count_Fetch[0].Users.length,
@@ -94,74 +93,68 @@ app.get(`/facebook`, async function (req, res) {
 })
 app.get(`/comments`, async function (req, res) {
   let Comments = await require('./models/Comments').find({})
-  if (Comments[0].Comments.length == 0) Comments[0].Comments = [`لا يوجد تعليقات`]
-  fetch('https://api.ipify.org/?format=json')
-    .then((response) => response.json())
-    .then(async (data) => {
-      res.render(`comments`, {
-        comments: Comments[0].Comments,
-        ipuser: data.ip,
-      })
-    })
+  res.render(`comments`, {
+    comments: Comments[0].Comments,
+    done: false,
+    err: false
+  })
+})
+app.get(`/masba7a`, async function (req, res) {
+  res.render(`masba7a`)
+})
+app.get(`/prophets-stories`, async function (req, res) {
+  res.render(`prophets-stories`)
+})
+app.get(`/prayer-timings`, async function (req, res) {
+  res.render(`prayer-timings`)
+})
+app.get(`/pillars-islam`, async function (req, res) {
+  res.render(`pillars-islam`)
+})
+app.get(`/pillars-faith`, async function (req, res) {
+  res.render(`pillars-faith`)
+})
+app.get(`/ahadeth`, async function (req, res) {
+  res.render(`ahadeth`)
 })
 
 
 app.post(`/comments`, async function (req, res) {
+  let Comments = await require('./models/Comments').find({})
   const webhookClient = new WebhookClient({ id: process.env.webhookId, token: process.env.webhookToken });
-  fs.readFile('comments.json', 'utf8', (err, data) => {
-    const comments = JSON.parse(data);
+  require('./models/Comments').updateOne({ ID: `1` }, { $push: { Comments: { user: req.body.name, email: req.body.email, comment: req.body.comment } } }, function (err) {
     if (err) {
       res.render(`comments`, {
-        comments,
+        comments: Comments[0].Comments,
         done: false,
         err: true
       })
       webhookClient.send({
         embeds: [new EmbedBuilder()
-          .setTitle(`Log Sending Comments To Islam`)
+          .setTitle(`Log Sending Comments To Islam Site`)
           .setURL(`https://www.islam.ml/comments`)
-          .setDescription(`حالة الإرسال : لم يتم الإرسال\nالإسم :\n${req.body.name}\nالبريد الإلكتروني :\n${req.body.email}\nالتعليق :\n${req.body.msg}\n\nError :\n\`\`\`${err}\`\`\``)
+          .setColor(`#b56400`)
+          .setDescription(`حالة الإرسال : لم يتم الإرسال\nالإسم :\n${req.body.name}\nالبريد الإلكتروني :\n${req.body.email}\nالتعليق :\n${req.body.comment}\n\nError :\n\`\`\`${err}\`\`\``)
         ]
       });
     }
-    let commentse = comments.map(m => m.email)
-    if (!commentse.includes(req.body.email)) {
-      comments.push({
-        username: req.body.name,
-        email: req.body.email,
-        msg: req.body.msg
-      });
-      var new_json = JSON.stringify(comments);
-      fs.writeFileSync('comments.json', new_json, 'utf8');
-      const data2 = fs.readFileSync("comments.json");
-      res.render(`comments`, {
-        comments,
-        done: true,
-        err: false
-      })
-      webhookClient.send({
-        embeds: [new EmbedBuilder()
-          .setTitle(`Log Sending Comments To Islam`)
-          .setURL(`https://www.islam.ml/comments`)
-          .setDescription(`حالة الإرسال : تم الإرسال\nالإسم :\n${req.body.name}\nالبريد الإلكتروني :\n${req.body.email}\nالتعليق :\n${req.body.msg}`)
-        ]
-      });
-    } else {
-      res.render(`comments`, {
-        comments,
-        done: false,
-        err: true
-      })
-      webhookClient.send({
-        embeds: [new EmbedBuilder()
-          .setTitle(`Log Sending Comments To Islam`)
-          .setURL(`https://www.islam.ml/comments`)
-          .setDescription(`حالة الإرسال : لم يتم الإرسال\nالإسم :\n${req.body.name}\nالبريد الإلكتروني :\n${req.body.email}\nالتعليق :\n${req.body.msg}\n\nError :\n\`\`\`البريد الإلكتروني موجود مسبقاً\`\`\``)
-        ]
-      });
-    }
-  });
+    Comments[0].Comments.push({ user: req.body.name, comment: req.body.comment })
+    res.render(`comments`, {
+      comments: Comments[0].Comments,
+      done: true,
+      err: false
+    })
+    webhookClient.send({
+      embeds: [new EmbedBuilder()
+        .setTitle(`Log Sending Comments To Islam Site`)
+        .setURL(`https://www.islam.ml/comments`)
+        .setColor(`#b56400`)
+        .setDescription(`حالة الإرسال : تم الإرسال\nالإسم :\n${req.body.name}\nالبريد الإلكتروني :\n${req.body.email}\nالتعليق :\n${req.body.comment}`)
+      ]
+    });
+  })
 })
+
 
 
 app.listen(8080, () => {
